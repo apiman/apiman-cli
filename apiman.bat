6e50c08fcdf4405a2b70d0fbfca27ea1684a52b0
Pete Cornish <outofcoffee@gmail.com>
2016-01-10 23:39:26 +0000

    Improves start scripts to check for existence of JAR.

--- apiman.bat
+++ apiman.bat
@@ -1,3 +1,10 @@
 @echo off
 
-java -jar .\build\libs\apiman-cli-1.0.0-SNAPSHOT-all.jar %*
+SET CLI_JAR=./build/libs/apiman-cli-1.0.0-SNAPSHOT-all.jar
+
+If Not Exist %CLI_JAR% (
+    call ./gradlew.bat clean shadowJar
+    cls
+)
+
+java -jar %CLI_JAR% %*
